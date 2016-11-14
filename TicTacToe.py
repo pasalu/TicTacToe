293660f290da8a2f787eb2d2b95e35bb892ab98d
Peter Salu <psalua@gmail.com>
2016-11-14 06:55:35 -0500

    Basic text only version of game.

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,13 +1,37 @@
-from Board import *
+from Board import Board
+
+def parse_input(value):
+    pieces = value.split(",")
+    x = int(pieces[0].strip())
+    y = int(pieces[1].strip())
+
+    return x, y
 
 def main():
     board = Board()
-    board.place(0, 0, board.X)
-    board.place(0, 1, board.X)
-    board.place(0, 2, board.X)
-    board.place(1, 1, board.O)
-    board.place(2, 2, board.O)
-    board.show()
+    print ("Welcome to Tic Tac Toe! We'll be starting you off with 2 player " 
+           "mode for now.")
+    player = board.X
+
+    while True:
+        coordinates = raw_input(player + " player Enter the x, y coordinates "
+                "of your piece: ")
+        x, y = parse_input(coordinates)
+
+        if not board.place(x, y, player):
+            print ("Enter coordinates that haven't been taken between 0 and 2.")
+            continue
+
+        board.show()
+
+        if board.has_won(player):
+            print (player + " Won!")
+            break
+
+        if player == board.X:
+            player = board.O
+        else:
+            player = board.X
 
 if __name__ == "__main__":
     main()
