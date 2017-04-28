3d7e7d34294d3d98c84edaf2851ffad1ea2d2e76
Peter Salu <psalua@gmail.com>
2017-04-28 06:53:41 -0400

    Place button on board only when the space is available.

--- BoardUI.py
+++ BoardUI.py
@@ -23,13 +23,17 @@ class BoardUI:
                 button = Button(master, width=BoardUI.BUTTON_WIDTH)
                 button.grid(row=y, column=x)
                 # Using partial here to freeze the value of button because it will be changed in the loop.
-                place_partial = partial(self.place, button)
+                place_partial = partial(self.place, button, x, y)
                 button.config(command=place_partial)
 
-    def place(self, button):
+    def place(self, button, x, y):
         """
         Place a button on the board.
         :param button: The button that was clicked.
+        :param x: The x coordinate of the button.
+        :param y: The y coordinate of the button.
         """
-        button.config(text=self.board.player)
-        self.board.switch_current_player()
+
+        if self.board.place(x, y, self.board.player):
+            button.config(text=self.board.player)
+            self.board.switch_current_player()
