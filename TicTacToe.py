9368fd058b2579d72f31748adac4a434035bae6a
Peter Salu <psalua@gmail.com>
2017-07-31 07:06:23 -0400

    Removed unused code.

--- TicTacToe.py
+++ TicTacToe.py
@@ -18,28 +18,5 @@ def main():
     root.mainloop()
     root.destroy()
 
-    print ("Welcome to Tic Tac Toe! We'll be starting you off with 2 player " 
-           "mode for now.")
-
-    while True:
-        coordinates = raw_input(player + " player Enter the x, y coordinates "
-                "of your piece: ")
-        x, y = parse_input(coordinates)
-
-        if not board.place(x, y, player):
-            print ("Enter coordinates that haven't been taken between 0 and 2.")
-            continue
-
-        board.show()
-
-        if board.has_won(player):
-            print (player + " Won!")
-            break
-
-        if player == board.X:
-            player = board.O
-        else:
-            player = board.X
-
 if __name__ == "__main__":
     main()
