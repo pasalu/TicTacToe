bc3fe9396c6bcb1affe41961a7f67af480290744
Peter Salu <psalua@gmail.com>
2018-05-04 17:24:46 -0500

    Cleaned up code by adding common function.

--- UI/BoardUI.py
+++ UI/BoardUI.py
@@ -37,7 +37,7 @@ class BoardUI(Frame):
                 button = Button(self, height=self.BUTTON_HEIGHT, width=self.BUTTON_WIDTH, font=button_font)
                 button.grid(row=y, column=x)
                 # Using partial here to freeze the value of button because it will be changed in the loop.
-                place_partial = partial(self.place, button, x, y)
+                place_partial = partial(self.button_click, button, x, y)
                 button.config(command=place_partial)
 
                 self.buttons[y].append(button)
@@ -51,7 +51,7 @@ class BoardUI(Frame):
         self.reset = Button(self, text="RESET", command=self.reset)
         self.reset.grid(row=4, column=0, columnspan=3)
 
-    def place(self, button, x, y):
+    def button_click(self, button, x, y):
         """
         Place a button on the board.
         :param button: The button that was clicked.
@@ -59,21 +59,24 @@ class BoardUI(Frame):
         :param y: The y coordinate of the button.
         """
         if self.board.place(x, y, self.board.player):
-            button.config(text=self.board.player)
-            self.board.switch_current_player()
-            self.set_label()
+            self.visual_place(button)
 
             if not self.board.is_multiplayer:
                 actions = self.board.get_actions()
                 action = self.ai.take_action(actions)
                 (ai_x, ai_y, ai_player) = action
-
                 self.board.place(ai_x, ai_y, ai_player)
-                ai_button = self.buttons[ai_y][ai_x]
-                ai_button.config(text=self.board.player)
 
-                self.board.switch_current_player()
-                self.set_label()
+                ai_button = self.buttons[ai_y][ai_x]
+                self.visual_place(ai_button)
+
+    def visual_place(self, button):
+        """
+        :param Button button:
+        """
+        button.config(text=self.board.player)
+        self.board.switch_current_player()
+        self.set_label()
 
     def set_label(self):
         """
